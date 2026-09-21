4cd4931728312b1b7e5ca02fe229fb67e81caf60
Samiul Basir Fahim <samiulbasirfahim.rxen@gmail.com>
2026-09-21 14:48:40 +0600

    feat: updated resume.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -8,76 +8,79 @@ import Text from "../ui/Text";
 import Link from "next/link";
 
 export default function Navbar() {
-  const path = usePathname();
+    const path = usePathname();
 
-  return (
-    <nav className="fixed top-0 right-0 z-[99] bg-blue-400 w-10 font-mono">
-      <motion.div whileHover={"hovered"} whileTap={"taped"} className="">
-        <motion.div className="w-dvw fixed top-0 left-0 flex justify-end h-dvw pointer-events-none ">
-          <motion.div
-            className="w-1/4 bg-black relative"
-            initial={{
-              height: 0,
-            }}
-            variants={{
-              taped: {
-                top: 0,
-                height: "50dvh",
-                transition: {
-                  duration: 0.2,
-                },
-              },
-              hovered: {
-                top: 0,
-                height: "40dvh",
-                transition: {
-                  duration: 0.3,
-                },
-              },
-            }}
-          />
-        </motion.div>
+    return (
+        <nav className="fixed top-0 right-0 z-[99] bg-blue-400 w-10 font-mono">
+            <motion.div whileHover={"hovered"} whileTap={"taped"} className="">
+                <motion.div className="w-dvw fixed top-0 left-0 flex justify-end h-dvw pointer-events-none ">
+                    <motion.div
+                        className="w-1/4 bg-black relative"
+                        initial={{
+                            height: 0,
+                        }}
+                        variants={{
+                            taped: {
+                                top: 0,
+                                height: "50dvh",
+                                transition: {
+                                    duration: 0.2,
+                                },
+                            },
+                            hovered: {
+                                top: 0,
+                                height: "40dvh",
+                                transition: {
+                                    duration: 0.3,
+                                },
+                            },
+                        }}
+                    />
+                </motion.div>
 
-        <motion.div
-          className="z-[99] rotate-90 text-xl font-semibold flex justify-end gap-4 top-10 fixed right-[-60px] lg:right-[-40px]"
-          animate={{
-            translateX: [60, 50, 60],
-            transition: {
-              duration: 2,
-              repeat: Infinity,
-              ease: "easeInOut",
-            },
-          }}
-          variants={{
-            taped: {
-              color: "var(--color-background)",
-            },
-            hovered: {
-              color: "var(--color-background)",
-            },
-          }}
-        >
-          <CustomLink href={path === "/works" ? "/" : "/works"}>
-            <Text>{path === "/works" ? "HOME" : "WORKS"}</Text>
-          </CustomLink>
+                <motion.div
+                    className="z-[99] rotate-90 text-xl font-semibold flex justify-end gap-4 top-10 fixed right-[-60px] lg:right-[-40px]"
+                    animate={{
+                        translateX: [60, 50, 60],
+                        transition: {
+                            duration: 2,
+                            repeat: Infinity,
+                            ease: "easeInOut",
+                        },
+                    }}
+                    variants={{
+                        taped: {
+                            color: "var(--color-background)",
+                        },
+                        hovered: {
+                            color: "var(--color-background)",
+                        },
+                    }}
+                >
+                    <CustomLink href={path === "/works" ? "/" : "/works"}>
+                        <Text>{path === "/works" ? "HOME" : "WORKS"}</Text>
+                    </CustomLink>
 
-          <motion.div
-            className="border-l-2"
-            variants={{
-              taped: {
-                borderColor: "var(--color-background)",
-              },
-              hovered: {
-                borderColor: "var(--color-background)",
-              },
-            }}
-          />
+                    <motion.div
+                        className="border-l-2"
+                        variants={{
+                            taped: {
+                                borderColor: "var(--color-background)",
+                            },
+                            hovered: {
+                                borderColor: "var(--color-background)",
+                            },
+                        }}
+                    />
 
-          <Link href="/Samiul_Basir_Fahim_Resume.pdf" download={'Samiul_Basir_Fahim_Resume.pdf'}>
-            RESUME
-          </Link>
-        </motion.div>
-      </motion.div>
-    </nav>
-  );
+                    <Link
+                        href="/samiul_basir_fahim_resume.pdf"
+                        download={"samiul_basir_fahim_resume.pdf"}
+                    >
+                        RESUME
+                    </Link>
+                </motion.div>
+            </motion.div>
+        </nav>
+    );
 }
